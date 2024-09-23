990407e8f9a8cdd012087b9f18a98eabdea7c181
Aidenzx <elpanacj64@gmail.com>
2024-09-23 00:12:57 -0500

    Update IPSnap.js

--- IPSnap.js
+++ IPSnap.js
@@ -1,70 +1,57 @@
 const axios = require('axios');
 const readline = require('readline');
 
-const title = `
-  _____ _____   _____ 
- |_   _|  __ \\ / ____|
-   | | | |__) | (___  _ __   __ _ _ __
-   | | |  ___/ \\___ \\| '_ \\ / _\` | '_ \\
-  _| |_| |     ____) | | | | (_| | |_) |
- |_____|_|    |_____/|_| |_|\\__,_| .__/
-                                 | |
-                                 |_|
-
-          BY: AIDENZX
-`;
+const colorMorado = (text) => `\x1b[35m${text}\x1b[0m`;
+const title = `${colorMorado('  _____ _____   _____ ')}\n${colorMorado(' |_   _|  __ \\ / ____|')}\n${colorMorado('   | | | |__) | (___  _ __   __ _ _ __')}\n${colorMorado('   | | |  ___/ \\___ \\| \'_ \\ / _\` | \'_ \\')}\n${colorMorado('  _| |_| |     ____) | | | | (_| | |_) |')}\n${colorMorado(' |_____|_|    |_____/|_| |_|\\__,_| .__/')}\n${colorMorado('                                 | |')}\n${colorMorado('                                 |_|')}\n\n${colorMorado('          BY: AIDENZX')}`;
 
 console.log(title);
-const apiKey = 'p6LyQpDU6CPketW3OovMyXJqRZ1cu35x';
+const token = '38d0f850d8b865';
+
 
 const getInfo = async (ip) => {
     try {
-        const response = await axios.post('https://api.iplogger.org/ip/info/', null, {
-            headers: {
-                'X-token': apiKey,
-                'Content-Type': 'multipart/form-data'
-            },
-            params: {
-                ip: ip
-            }
-        });
-        
-        const data = response.data;
-
+        const response = await axios.get(`https://ipinfo.io/${ip}/json?token=${token}`);
         console.log('\nInformación de la IP:');
+        const data = response.data;
         console.log(`IP: ${data.ip}`);
-        console.log(`Continente: ${data.continent}`);
-        console.log(`País: ${data.country}`);
+        console.log(`Hostname: ${data.hostname}`);
         console.log(`Ciudad: ${data.city}`);
-        console.log(`Latitud: ${data.latitude}`);
-        console.log(`Longitud: ${data.longitude}`);
-        console.log(`Exactitud: ${data.accuracy}`);
-        console.log(`Estado/Región: ${data.region}`);
-        console.log(`Distrito/Condado: ${data.district}`);
+        console.log(`Región: ${data.region}`);
+        console.log(`País: ${data.country}`);
+        const [latitud, longitud] = data.loc.split(',');
+        console.log(`Latitud: ${latitud}`);
+        console.log(`Longitud: ${longitud}`);
         console.log(`Código Postal: ${data.postal}`);
         console.log(`Zona Horaria: ${data.timezone}`);
-        console.log(`Hora Local: ${data.local_time}`);
-        console.log(`ISP: ${data.isp}`);
-        console.log(`Organización: ${data.organization}`);
-        console.log(`Conexión: ${data.connection}`);
-        console.log(`Unión Europea: ${data.eu}`);
-        console.log(`Estación Meteorológica: ${data.weather_station}`);
+        console.log(`ASN: ${data.asn.asn} (${data.asn.name})`);
+        console.log(`Organización: ${data.company.name}`);
 
+        if (data.abuse) {
+            console.log(`\nInformación de abuso:`);
+            console.log(`Dirección: ${data.abuse.address}`);
+            console.log(`Correo: ${data.abuse.email}`);
+            console.log(`Teléfono: ${data.abuse.phone}`);
+        }
+
+        
         process.exit(0);
     } catch (error) {
         console.error('Error al obtener la información de la dirección IP:', error.message);
     }
 };
 
+
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
+
 const isValidIP = (ip) => {
     const regex = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
     return regex.test(ip);
 };
+
 
 const preguntarIP = () => {
     rl.question('Por favor, ingresa la dirección IP: ', (ipToLookup) => {
@@ -73,9 +60,13 @@ const preguntarIP = () => {
             console.log('Ejemplo de IP válida: 203.0.113.123');
             preguntarIP();
         } else {
-            getInfo(ipToLookup).then(() => {});
+            
+            getInfo(ipToLookup).then(() => {
+                
+            });
         }
     });
 };
+
 
 preguntarIP();
